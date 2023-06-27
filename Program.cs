9eaffc1b0841682e984aa4d8b0a05104340c2b22
Burak Aydin <burak611aydin@gmail.com>
2023-06-27 19:32:19 +0300

    loki's default teleport chance ıs edited

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Fight1v1
         static void Main(string[] args)
         {
             Warrior thor = new StrenghtWarrior("Thor", 100, 26, 10);
-            Warrior loki = new MagicWarrior("Loki", 75, 20, 10, 60);
+            Warrior loki = new MagicWarrior("Loki", 75, 20, 10, 75);
 
             Battle.StartFight(thor, loki);
             Console.ReadLine(); 
